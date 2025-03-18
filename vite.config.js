37f993699827ab0d74725d69fe46252daafac88d
Yuhan Wang <wyuhan18@gmail.com>
2025-03-19 03:53:37 +1030

    changed base in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
+  base: process.env.VITE_BASE_PATH || "/yuhan-portfolio",
   server: {
     proxy: {
       "/api": {
